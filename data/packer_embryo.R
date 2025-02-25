cc4666140baa8f799b71c0d51d9ba743870fcc15
Hinna He <hinna@i.smu.edu.cn>
2025-02-25 21:16:18 +0800

    Update packer_embryo.R

--- data/packer_embryo.R
+++ data/packer_embryo.R
@@ -1,30 +1,65 @@
-## preprocessing the example data and convert it into SingleCellExperiment object
+## preprocessing data
+##
+## This script performs preprocessing on raw SingleCellExperiment data using sclet function for downstream analysis. 
+##
+## Input:
+## - Raw gene expression data (exp) in the form of a matrix
+## - Cell metadata (cell) in the form of a DataFrame
+## - Gene annotation (gene) in the form of a DataFrame
 ## The original data is from https://github.com/cole-trapnell-lab/monocle3/blob/master/examples/c_elegans_embryo.R
-
-## load data
-expression_matrix <- readRDS("packer_embryo_expression.rds")
-cell_metadata <- readRDS("packer_embryo_colData.rds")
-gene_annotation <- readRDS("packer_embryo_rowData.rds")
-
-## create cds object
-cds <- new_cell_data_set(expression_matrix,
-                         cell_metadata = cell_metadata,
-                         gene_metadata = gene_annotation)
-
-## preprocess data
-cds <- preprocess_cds(cds, num_dim = 50)
-cds <- align_cds(cds, alignment_group = "batch", residual_model_formula_str = "~ bg.300.loading + bg.400.loading + bg.500.1.loading + bg.500.2.loading + bg.r17.loading + bg.b01.loading + bg.b02.loading")
-cds <- reduce_dimension(cds)
+##
+## Output:
+## - Processed SCE object saved as embryo_sce.rds
 
 
-## convert them into SCE object
+## load raw data
+rm(list = ls())
+exp <- readRDS("./packer_embryo_expression.rds")
+cell <- readRDS("packer_embryo_colData.rds")
+gene <- readRDS("packer_embryo_rowData.rds")
+
+## load packages
 library(SingleCellExperiment)
-sce <- SingleCellExperiment(assays = list(counts = expression_matrix),
-                            colData = cell_metadata, 
-                            rowData = gene_annotation)
+library(yulab.utils)
+library(ggplot2)
+source("./seurat.R")
+source("./subset.R")
 
-reducedDims(sce)[["UMAP"]] <- reducedDim(cds, "UMAP")
-rownames(sce@assays@data$counts) <- gene_annotation[['gene_short_name']]
-rownames(sce) <- gene_annotation[['gene_short_name']]
+## create raw SCE object
+embryo_sce <- SingleCellExperiment(assay = list(counts = exp), colData = cell, rowData = gene)
+rownames(embryo_sce@assays@data$counts) <- gene[['gene_short_name']]
+rownames(embryo_sce) <- gene[['gene_short_name']]
 
-saveRDS(sce, "packer_embryo.rds")
+# QC
+embryo_sce[["percent.mt"]] <- PercentageFeatureSet(embryo_sce, "^MT-")
+subset_feature(embryo_sce, 10)
+embryo_sce <- subset_feature(embryo_sce, mincell = 3, peek=FALSE)
+
+# Normalization
+embryo_sce <- NormalizeData(embryo_sce)
+
+# correct batch effect using "batch" factor in ColData(embryo_sce)
+embryo_sce_correct <- batchelor::quickCorrect(embryo_sce, batch = embryo_sce$batch)
+corrected_logcounts <- assay(embryo_sce_correct$corrected, "reconstructed")
+corrected_logcounts_dgc <- as(corrected_logcounts, "dgCMatrix")
+assays(embryo_sce)$logcounts <- corrected_logcounts_dgc
+
+# find variable features
+embryo_sce <- FindVariableFeatures(embryo_sce)
+
+# dimension reduction
+embryo_sce <- ScaleData(embryo_sce)
+embryo_sce <- scater::runPCA(embryo_sce, subset_row = VariableFeatures(embryo_sce), exprs_values = "scaled")
+ElbowPlot(embryo_sce)
+
+# clustering
+set.seed(1234)
+embryo_sce <- FindNeighbors(embryo_sce, dims = 1:20)
+embryo_sce <- FindClusters(embryo_sce)
+
+# UMAP
+embryo_sce <- RunUMAP(embryo_sce, 1:20)
+ggsc::sc_dim(embryo_sce, reduction = "UMAP") + ggsc::sc_dim_geom_label()
+
+saveRDS(embryo_sce, "./embryo_sce.rds")
+#usethis::use_data(embryo_sce, overwrite = TRUE)
